95aae3d32cc4b6d55ecaa76cb93b827180d2a5c0
Deepak Kumbhar <Deepakk@emids.com>
2022-09-25 21:45:37 +0530

    added process date by dob & country

--- src/main/java/com/emids/springbatch/config/CustomerProcessor.java
+++ src/main/java/com/emids/springbatch/config/CustomerProcessor.java
@@ -3,7 +3,6 @@ package com.emids.springbatch.config;
 import com.emids.springbatch.entity.Customer;
 import org.springframework.batch.item.ItemProcessor;
 
-import javax.xml.crypto.Data;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,18 +12,24 @@ public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
 
     @Override
     public Customer process(Customer customer) throws Exception {
+//        if(customer.getCountry().equals("United States")) {
+//            return customer;
+//        }else{
+//            return null;
+//        }
 
-        String str_date = customer.getDob();
-        DateFormat formatter = new SimpleDateFormat("dd-MM-yy");
-        Date date = formatter.parse(str_date);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
+//        String str_date = customer.getDob();
+//        DateFormat formatter = new SimpleDateFormat("dd-MM-yy");
+//        Date date = formatter.parse(str_date);
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(date);
+//        if(calendar.get(Calendar.YEAR)>2000) {
+//            return customer;
+//        }else{
+//            return null;
+//        }
 
-        if (calendar.get(Calendar.YEAR) > 2000) {
-            return customer;
-        } else {
-            return null;
-        }
-        //return customer;
+
+        return customer;
     }
 }
